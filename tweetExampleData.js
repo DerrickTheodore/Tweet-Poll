b4d18414a2813430def64ad8cdfb130b4eaddc0f
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-06-17 08:46:19 -0400

    modify tweet example data

--- tweetExampleData.js
+++ tweetExampleData.js
@@ -1,3 +1,7 @@
+const asset = {
+    tweetBody: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, amet distinctio! Ab autem nam harum dolorem rem? Laboriosam ullam iusto esse enim sequi? Voluptas quod at quisquam nostrum laudantium aspernatur."
+}
+
 module.exports = [
     {
         // score: sentiment(tweet).score,
@@ -5,8 +9,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 0},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum (NEUTRAL)",
-        user_name: "test:name",
+        tweetBody: `(NEUTRAL) ${asset.tweetBody}`,
+        user_name: "User_01",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },
@@ -16,8 +20,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: -1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Negative)",
-        user_name: "test:name",
+        tweetBody: `(NEGATIVE) ${asset.tweetBody}`,
+        user_name: "User_02",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },
@@ -27,8 +31,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(POSITIVE) ${asset.tweetBody}`,
+        user_name: "USER_03",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },
@@ -38,8 +42,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: -1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(NEGATIVE) ${asset.tweetBody}`,
+        user_name: "USER_04",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },
@@ -49,8 +53,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: -1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(NEGATIVE) ${asset.tweetBody}`,
+        user_name: "USER_05",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },    {
@@ -59,8 +63,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 0},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(NETURAL) ${asset.tweetBody}`,
+        user_name: "USER_06",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },    {
@@ -69,8 +73,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(POSITIVE) ${asset.tweetBody}`,
+        user_name: "USER_07",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },    {
@@ -79,8 +83,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(POSITIVE) ${asset.tweetBody}`,
+        user_name: "USER_08",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },    {
@@ -89,8 +93,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: -1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(NEGATIVE) ${asset.tweetBody}`,
+        user_name: "USER_09",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },    {
@@ -99,8 +103,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 1},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(POSITIVIE) ${asset.tweetBody}`,
+        user_name: "USER_10",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     },    {
@@ -109,8 +113,8 @@ module.exports = [
         timeStamp: "test:time",
         sentimentDataObject: {score: 0},
         // if tweet has been retweeted, its full text lives in the retweeted_status object
-        tweetBody: "Lorem Ipsum! (Positive)",
-        user_name: "test:name",
+        tweetBody: `(NETURAL) ${asset.tweetBody}`,
+        user_name: "USER_11",
         user_location: "test:location",
         avatar_url: "./images/twitter-profile.jpeg"
     }
